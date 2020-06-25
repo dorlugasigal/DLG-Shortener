d473f73bcc89482a59a42637884aba46c0a51a62
Dor Lugasi-Gal <dorlugasigal@gmail.com>
2020-06-25 22:33:29 +0300

    added redirect response

--- DLG-Shortner/Controllers/WeatherForecastController.cs
+++ DLG-Shortner/Controllers/WeatherForecastController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -24,16 +26,9 @@ namespace DLG_Shortner.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Redirect("https://weather.com/he-IL/weather/today/l/ISXX0010:1:IS");
         }
     }
 }
